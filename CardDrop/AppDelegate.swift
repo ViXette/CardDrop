917ba036acd59de10325f9c8e005648405f07d30
ViXette <v.xette@gmail.com>
2018-11-14 18:47:04 +1000

    Initial commit

---
+++ CardDrop/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  CardDrop
+//
+//  Created by ViXette on 14/11/2018.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		// Override point for customization after application launch.
+		return true
+	}
+
+}
---
+++ CardDrop/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CardDrop
+//
+//  Created by ViXette on 14/11/2018.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+}
